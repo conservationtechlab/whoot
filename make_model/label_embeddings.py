ede350ff718bca7c79b7a5a9782d02516c61f206
Katie Garwood <kgarwood@sdzwa.org>
2025-01-20 16:19:26 -0800

    pseudo code added
    made the rough outline for making embeddings file and labels one

--- make_model/label_embeddings.py
+++ make_model/label_embeddings.py
@@ -13,7 +13,8 @@ per 3 second chunk.
 python label_embeddings.py /path/to/output/ /path/to/birdnet_embeddings/ /path/to/desired/outputs/
 """
 import argparse
-import
+import pandas as pd
+import os
 
 
 def main(human_labels, embeddings, output):
@@ -25,7 +26,33 @@ def main(human_labels, embeddings, output):
         output:
 
     """
-    
+    for filename in os.listdir(human_labels):
+        file_path = os.path.join(human_labels, filename)
+        df = pd.read_csv(file_path)
+        df.add 1024 empty columns
+        filename = strip _chunks.csv
+        for birdnet in os.listdir(embeddings):
+            birdnet_path = os.path.join(embeddings, birdnet)
+            dfb = pd.read(birdnet_path)
+            strip first 2 columns
+            if check num rows -1 equals num rows in df:
+                fill the 1024 column info from dfb into df-
+                save the file to new df in output%03.csv
+                print(f"Labeled embeddings created for: {output_filename}") 
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Input Directory Path'
+        )
+    parser.add_argument('labels', type=str,
+                        help='Directory path to human labeled csvs')
+    parser.add_argument('embeddings', type=str,
+                        help='Directory path to birdnet embeddings')
+    parser.add_argument('output', type=str,
+                        help='Directory path to desired output csvs')
+    args = parser.parse_args()
+    main(args.labels, args.embeddings, args.output)
 
 
 
