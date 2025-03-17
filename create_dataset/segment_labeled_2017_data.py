8fff3297569a41c6f2787ca1dc610fd846da7581
Katie Garwood <kgarwood@sdzwa.org>
2025-03-17 12:07:22 -0700

    add no creation of no bird label segments
    now it will create a folder of 3 second bird detection segments,
    as well as a folder from the same wavs with no bird detection
    segments

--- create_dataset/segment_labeled_2017_data.py
+++ create_dataset/segment_labeled_2017_data.py
@@ -91,6 +91,59 @@ def create_no_bird_segments(labels, wavs, output):
     """Create no bird call audio segments.
 
     """
+    os.makedirs(output, exist_ok=True)
+
+    scored_data = pd.read_csv(labels)
+    output = output + "no_bird_sounds/"
+    os.makedirs(output, exist_ok=True)
+
+    for audio_file in os.listdir(wavs):
+        if audio_file.endswith('.wav'):
+            audio_path = os.path.join(wavs, audio_file)
+
+            try:
+                time_series, sample_rate = librosa.load(audio_path, sr=None)
+                audio_duration = librosa.get_duration(y=time_series, sr=sample_rate)
+            except Exception as err:
+                print(f"Error processing {audio_file}: {err}")
+                continue
+
+            total_chunks = int(audio_duration // 3) + 1
+            chunks_data = {
+                'Chunk Start': [i * 3 for i in range(total_chunks)],
+                'Chunk End': [(i + 1) * 3 for i in range(total_chunks)],
+                'Label': ['no'] * total_chunks
+            }
+            chunks_df = pd.DataFrame(chunks_data)
+
+            filtered_data = scored_data[scored_data['IN FILE'] == audio_file]
+
+            for _, row in filtered_data.iterrows():
+                if row['TOP1MATCH'] != 'null':
+                    start_time = float(row['OFFSET'])
+                    end_time = start_time + float(row['DURATION'])
+
+                    for i in range(len(chunks_df)):
+                        chunk_start = chunks_df.loc[i, 'Chunk Start']
+                        chunk_end = chunks_df.loc[i, 'Chunk End']
+                        if start_time < chunk_end and end_time > chunk_start:
+                            chunks_df.loc[i, 'Label'] = 'bird'
+
+            bird_sound = AudioSegment.from_wav(audio_path)
+            segment_index = 0
+            for i in range(len(chunks_df)):
+                if chunks_df.loc[i, 'Label'] == 'no':
+                    chunk_start = chunks_df.loc[i, 'Chunk Start'] * 1000
+                    chunk_end = chunks_df.loc[i, 'Chunk End'] * 1000
+                    segment = bird_sound[chunk_start:chunk_end]
+
+                    output_file = os.path.join(
+                        output, f'{os.path.splitext(audio_file)[0]}_nobird_segment_{segment_index}.wav'
+                    )
+                    segment.export(output_file, format='wav')
+                    segment_index += 1
+
+    print("Processing complete!")
 
 def main(labels, wavs, output):
     """Run main script
