bea4cf4c31a489dfcf36fd0b5576005d6e2ad8ba
Katie Garwood <kgarwood@sdzwa.org>
2025-04-24 16:39:27 -0700

    starting to figure out the problem
    not really working code but beginning to think about how to keep
    the groups together while properly creating stratified datasets
    found a tutorial that deals with the same problem

--- create_dataset/strat_k_folds.py
+++ create_dataset/strat_k_folds.py
@@ -6,19 +6,35 @@ import argparse
 def create_strat_folds(df):
     """
     """
-    obtain complete class distribution for all classes
-    divide each class by 5, thats how many need to be in each fold
-    lets say theres 5000 bo buow, 1000 cluck, 500 coocoo, 500 chick begging, 2000 alarm, 1000 twitter
-    so each fold needs, 1000 no buow, 100 coocoo, 100 chick begging, 400 alarm, 200 twitter
+    cluck_df = df[df['label'] == 'cluck']
+    coocoo_df = df[df['label'] == 'coocoo']
+    twitter_df = df[df['label'] == 'twitter']
+    alarm_df = df[df['label'] == 'alarm']
+    chick_beg_df = df[df['label'] == 'chick_begging']
+    no_buow_df = df[df['label'] == 'no_buow']
+    # amount needed per fold
+    print(f"{chick_beg_df}")
+    len_cluck = len(cluck_df) / 5
+    len_coocoo = len(coocoo_df) / 5
+    len_twitter = len(twitter_df) / 5
+    len_alarm = len(alarm_df) / 5
+    len_chick = len(chick_beg_df) / 5
+    len_no_buow = len(no_buow_df) / 5
+    print(f"len_cluck: {len_cluck} len_coocoo: {len_coocoo} len_twitter: {len_twitter} len_alarm: {len_alarm} len_chick: {len_chick} len_no_buow: {len_no_buow}")
     
-    if i create a df that has the wav path, the distribution of calls in a column. i can randomly add different ones
+
+    grouped = df.groupby('original_path')
+    for index, group in grouped:
+        print(f"group {index}")
+        print(group)
+    '''if i create a df that has the wav path, the distribution of calls in a column. i can randomly add different ones
     together until i get about that disribution per class. then i can go in and add a column to the df that has the fold it's in. donezo
-    what combination of these properites will give me the closest distribution to a balanced set? 
+    what combination of these properites will give me the closest distribution to a balanced set?''' 
 
 def main(meta):
     """
     """
-    df = pd.read(meta)
+    df = pd.read_csv(meta)
     create_strat_folds(df)
 
 
