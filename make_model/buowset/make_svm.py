98954527f57146eefeb83b3513028072beb98c36
Katie Garwood <kgarwood@sdzwa.org>
2025-05-14 16:35:45 -0700

    pylint and flake8

--- make_model/buowset/make_svm.py
+++ make_model/buowset/make_svm.py
@@ -19,10 +19,8 @@ If you would like to save our your resulting model file, add
 
 """
 import argparse
-import os
-import glob
-import ntpath
 import pickle
+import sys
 import pandas as pd
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report
@@ -100,7 +98,10 @@ def make_svm(meta, embeddings_df):
     Args:
         meta (str): the metadata file containing fold and label id as an int
 
-        embeddings_df (str): the path to your embeddings folds/files
+        embeddings_df (str): the path to your embeddings folds/files.
+
+    Returns:
+        svm (model): Support vector machine model.
     """
     x_train, y_train, x_test, y_test = make_x_and_y(meta, embeddings_df)
     print("beginning model training")
@@ -112,9 +113,21 @@ def make_svm(meta, embeddings_df):
     print("Classification report with default threshold:")
     print(classification_report(y_test, y_pred_default))
 
+    return svm
+
 
 def save_out_model(svm, model_file):
-    """
+    """Saves model as a pkl.
+
+    If you'd like to save the model to use, you can optionally
+    provide a -model_file arg string. If you are just wanting
+    to see how the model metrics are or testing code, you
+    may not want to save the model each time.
+
+    Args:
+        svm (model): The support vector machine created.
+
+        model_file (str): Path to where the model will be saved .pkl.
     """
     with open(model_file, 'wb') as file:
         pickle.dump(svm, file)
@@ -122,7 +135,7 @@ def save_out_model(svm, model_file):
     print(f"Saved model path: {model_file}")
 
 
-def main(meta, embeds, source, df, model_file):
+def main(meta, embeds, source, embed_df, model_file):
     """Main script to run
 
     Args:
@@ -133,16 +146,16 @@ def main(meta, embeds, source, df, model_file):
         source (str): What format your embeddings are in (currently
                       either perch or birdnet).
 
-        df (str): If you have a premade dataframe with the keys
+        embed_df (str): If you have a premade dataframe with the keys
                              as the filename and the values as the embedding
-                             list, use this argument and leave embeds and source
-                             empty.
+                             list, use this argument and leave embeds and
+                             source empty.
 
         model_file (str): Path to desired model output file, must be a .pkl.
     """
     metadata = pd.read_csv(meta, index_col=0)
-    if df == False:
-        if embeds == False:
+    if embed_df is not True:
+        if embeds is not True:
             print("Found no path to embeddings folder/file. Please add "
                   "-embeds arg when running script.")
             sys.exit()
@@ -152,15 +165,17 @@ def main(meta, embeds, source, df, model_file):
             embeddings_df = obtain_perch_embeddings(embeds)
         else:
             print(f"Cannot create embeddings without knowing the source,"
-                   " ensure you selected -source perch or birdnet")
+                  " ensure you selected -source perch or birdnet")
             sys.exit()
     else:
-        embeddings_df = pd.read_csv(df)
+        embeddings_df = pd.read_csv(embed_df)
 
-    svm = make_svm(metadata, embeddings_df)
+    if model_file is not True:
+        svm = make_svm(metadata, embeddings_df)
+        save_out_model(svm, model_file)
+    else:
+        make_svm(metadata, embeddings_df)
 
-    if model_file == False:
-        save_out_model(svm)
 
 if __name__ == "__main__":
     PARSER = argparse.ArgumentParser(
@@ -172,9 +187,9 @@ if __name__ == "__main__":
                         help='Path to directory with embeddings files.')
     PARSER.add_argument('-source', type=str, default=False,
                         help='Source of embeddings (birdnet or perch).')
-    PARSER.add_argument('-df', type=str, default=False,
+    PARSER.add_argument('-embed_df', type=str, default=False,
                         help='Path to your premade embeddings dataframe.')
     PARSER.add_argument('-model_file', type=str,
                         help='File name and location of saved model.pkl.')
     ARGS = PARSER.parse_args()
-    main(ARGS.meta, ARGS.embeds, ARGS.source, ARGS.df, ARGS.model_file)
+    main(ARGS.meta, ARGS.embeds, ARGS.source, ARGS.embed_df, ARGS.model_file)
