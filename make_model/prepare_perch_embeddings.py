d19829d477ffd48cd7bc365338e560c225ead2cf
Sumega Mandadi <sumega.mandadi@gmail.com>
2025-05-15 14:05:58 -0700

    Change output dataframe format for standardization

--- make_model/prepare_perch_embeddings.py
+++ make_model/prepare_perch_embeddings.py
@@ -26,36 +26,7 @@ import os
 from perch_hoplite.db import sqlite_usearch_impl
 
 
-def split_base_segment(filename):
-    '''separates source file base and segment #
-    example: audio_segment_3.wav -> audio, 3
-
-    Args:
-        filename: segment file name
-
-    Returns:
-        source file base name & segment #
-    '''
-
-    base, _ = os.path.splitext(filename)
-    return base.split('_segment_')
-
-def get_start_stop(seg_id):
-    ''' calculates start stop timestamp in s
-        from segment number
-
-    Args:
-        seg_id (int): segment number of audio chunk
-
-    Returns:
-        start s, stop s
-
-    '''
-    seg_id = int(seg_id)
-    return (seg_id*3, (seg_id+1)*3)
-
-
-def main(sqlite_dir, label, output_dir):
+def main(sqlite_dir, output_dir, embeddings_description):
     '''
     runs main script
     '''
@@ -70,23 +41,19 @@ def main(sqlite_dir, label, output_dir):
     for i in range(n_embeddings):
 
         file_name = db.get_embedding_source(i+1).source_id
-        base_name, segment_id = split_base_segment(file_name)
-        start, stop = get_start_stop(segment_id)
-        base_dict = {'start': start,
-                     'stop': stop,
-                     'label': label}
+
+        base_dict = {'filename': file_name}
 
         embedding = db.get_embedding(i+1)
-        embedding_dict = {f'feature_{j}': val for j, val in enumerate(embedding)}
+        embedding_dict = {f'{j}': val for j, val in enumerate(embedding)}
 
         full_row = {**base_dict, **embedding_dict}
 
         master_data.append(full_row)
 
-
     master_df = pd.DataFrame(master_data)
-    csv_filename = f'{output_dir}/{label}_embeddings_forSVM.csv'
-    master_df.to_csv(csv_filename)
+    csv_filename = os.path.join(output_dir, f'{embeddings_description}_perch_embeddings.csv')
+    master_df.to_csv(csv_filename, index=False)
 
     print('Complete!')
     print(f'Saved at:\n\t{csv_filename}')
@@ -95,15 +62,15 @@ def main(sqlite_dir, label, output_dir):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(
-                  description='Input SQLite Direcotry and Label')
+                  description='Input Perch Embeddings sqlite database and output directory')
 
     parser.add_argument('sqlite_dir', type=str,
                         help='Path to directory that contains '
                              'hoplite.sqlite and usearch.index')
-    parser.add_argument('label', type=str,
-                        help='Label for all embeddings in given db')
     parser.add_argument('output_dir', type=str,
                         help='Directory for output file')
+    parser.add_argument('embeddings_description', type=str,
+                        help='Name of embeddings group')
 
     args = parser.parse_args()
-    main(args.sqlite_dir, args.label, args.output_dir)
+    main(args.sqlite_dir, args.output_dir, args.embeddings_description)
