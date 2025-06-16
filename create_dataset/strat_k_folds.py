e21482cfd622f7906ae85e0971a0b1cde076a46f
Katie Garwood <kgarwood@sdzwa.org>
2025-06-16 14:40:02 -0700

    fixed pylint and flake8 and docstrings

--- create_dataset/strat_k_folds.py
+++ create_dataset/strat_k_folds.py
@@ -1,15 +1,32 @@
+"""Split buowset into stratified k-folds.
+
+Groups detections from the same wav file into 'groups'
+and then determines the overall class distribution and
+the class distribution for each 'group'. It allocates
+all the groups to a 'fold' in a way where the folds
+are roughly the same class distribution as the overall
+dataset.
+
+Usage:
+    python3 strat_k_folds.py /path/to/metadata.csv
 """
-"""
-from k_fold_split_copy import calculate_cost, generate_search_space
-from k_fold_split_copy import solution_to_str, generate_initial_solution
-from k_fold_split_copy import solve, select_move
-import pandas as pd
 import argparse
+import pandas as pd
 import numpy as np
 
 
+from k_fold_split_copy import solve
+
+
 def create_strat_folds(df):
-    """
+    """Create grouped stratified k-folds.
+
+    Args:
+        df (pd.Dataframe): The metadata csv from when the dataset was created.
+
+    Returns:
+        pd.DataFrame: The same metadata but with labels as ints and a new fold
+            column to denote the fold that segment is apart of.
     """
     num_classes = 6
     original_df = df
@@ -30,14 +47,17 @@ def create_strat_folds(df):
             counts[int(label)] = count
         group_matrix.append(counts)
         group_names.append(index)
-    print(group_names)
     problem = np.array(group_matrix)
-    print(problem)
     solution = solve(problem, k=5, verbose=True)
+    # the fold allocation for each 'group'
     print(f"solution {solution}")
     print(np.sum(problem, axis=0) / np.sum(problem))
     folds = [problem[solution == i] for i in range(5)]
-    fold_percents = np.array([np.sum(folds[i], axis=0) / np.sum(folds[i]) for i in range(5)])
+    fold_percents = np.array(
+        [np.sum(folds[i], axis=0) / np.sum(folds[i]) for i in range(5)]
+    )
+    # the % of each class in each fold
+    print(f"Fold percents: {fold_percents}")
     print(folds)
     grouped_original = original_df.groupby('original_path')
     df_with_folds = pd.DataFrame()
@@ -48,15 +68,19 @@ def create_strat_folds(df):
         count += 1
     return df_with_folds
 
+
 def main(meta):
-    """
+    """Execute main script.
+
+    Args:
+        meta (str): Path to metadata csv from creating the dataset.
     """
     df = pd.read_csv(meta, index_col=0)
     df_with_folds = create_strat_folds(df)
     df_with_folds.to_csv("5-fold_meta.csv")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Input Directory Path'
         )
@@ -64,4 +88,3 @@ if __name__=="__main__":
                         help='Path to metadata csv')
     args = parser.parse_args()
     main(args.meta)
-
