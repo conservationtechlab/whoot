3f0b20476ed7bf9b3bcc4240ecd1cbc750687b2a
Katie Garwood <kgarwood@sdzwa.org>
2025-06-18 16:30:40 -0700

    add docstrings, lint, and remove unused functions

--- create_dataset/k_fold_split_copy.py
+++ create_dataset/k_fold_split_copy.py
@@ -1,40 +1,38 @@
 """Optimizing k-fold splits with groups.
 
-Downloaded and modified from https://github.com/joaofig/strat-group-split/tree/main
+These functions aid strat_k_folds.py in calculating the
+optimal fold allocation for all the groups in the dataset
+ensuring the folds are as equal in size as they can be,
+while also being as close to the actual class distribution
+as possible.
+
+Downloaded and modified from:
+https://github.com/joaofig/strat-group-split/tree/main
 """
-
+from typing import Set, Tuple
 import numpy as np
-
 from numpy.random import default_rng
 from numba import njit
-from typing import Set, Tuple
-
-
-def generate_problem(num_groups: int,
-                     num_classes: int,
-                     min_group_size: int,
-                     max_group_size: int,
-                     class_percent: np.array) -> np.ndarray:
-
-    problem = np.zeros((num_groups, num_classes), dtype=int)
-
-    rng = default_rng()
-    group_sizes = rng.integers(low=min_group_size,
-                               high=max_group_size,
-                               size=num_groups)
-
-    for i in range(num_groups):
-        # Calculate the
-        proportions = np.random.normal(class_percent, class_percent / 10)
-
-        problem[i, :] = proportions * group_sizes[i]
-    return problem
 
 
 @njit
 def calculate_cost(problem: np.ndarray,
                    solution: np.ndarray,
                    k: int) -> float:
+    """Calculate difference of current solution to optimal solution.
+
+    Args:
+        problem (np.array): A matrix with a column per class and the
+            class counts for each group as the values.
+        solution (np.ndarray): A 1D array where each value is the current
+            fold allocation for the corresponding group.
+        k (int): Number of folds.
+
+    Returns:
+        float: The summation of the differences between the folds'
+            class distributions from the optimal class distribution, and the
+            size of the folds to the size the folds should be.
+    """
     cost = 0.0
     total = np.sum(problem)
     class_sums = np.sum(problem, axis=0)
@@ -54,7 +52,8 @@ def calculate_cost(problem: np.ndarray,
             if fold_sum == 0:
                 cost += (0 - class_sums[j] / total) ** 2
             else:
-                cost += (np.sum(problem[idx, j]) / fold_sum - class_sums[j] / total) ** 2
+                sum_problem = np.sum(problem[idx, j]) / fold_sum
+                cost += (sum_problem - class_sums[j] / total) ** 2
     return cost
 
 
@@ -62,6 +61,19 @@ def calculate_cost(problem: np.ndarray,
 def generate_search_space(problem: np.ndarray,
                           solution: np.ndarray,
                           k: int) -> np.ndarray:
+    """Generate the search space.
+
+    Args:
+        problem (np.ndarray): A matrix with a column per class and the
+            class counts for each group as the values.
+        solution (np.ndarray): The last known solution.
+        k (int): Number of folds.
+
+    Returns:
+        np.ndarray: The search space. Folds as columns and cost values
+            for each group with a placeholder in one fold each to allow for
+            a cost calculation relative to the placeholder.
+    """
     num_groups = problem.shape[0]
 
     space = np.zeros((num_groups, k))
@@ -70,7 +82,7 @@ def generate_search_space(problem: np.ndarray,
     for i in range(num_groups):
         for j in range(k):
             if solution[i] == j:
-                space[i,j] = np.inf
+                space[i, j] = np.inf
             else:
                 sol[i] = j
                 space[i, j] = calculate_cost(problem, sol, k)
@@ -80,12 +92,32 @@ def generate_search_space(problem: np.ndarray,
 
 @njit
 def solution_to_str(solution: np.ndarray) -> str:
+    """Convert the solution to a string.
+
+    Args:
+        solution (np.ndarray): The current solution.
+    Returns:
+        str: The current solution as a string.
+    """
     return "".join([str(n) for n in solution])
 
 
 def generate_initial_solution(problem: np.ndarray,
                               k: int,
-                              algo: str="k-bound") -> np.ndarray:
+                              algo: str = "k-bound") -> np.ndarray:
+    """Generate the first solution.
+
+    Args:
+        problem (np.array): A matrix with a column per class and the
+            class counts for each group as the values.
+        k (int): The number of folds.
+        algo (str): Method for creating initial solution. Defaults to a
+            greedy algorithm to satisfy fold proportion requirements only.
+
+    Returns:
+        np.ndarray: A 1D array where each value is the current fold
+            allocation for the corresponding group.
+    """
     num_groups = problem.shape[0]
     if algo == "k-bound":
         rng = default_rng()
@@ -93,16 +125,16 @@ def generate_initial_solution(problem: np.ndarray,
         indices = rng.permutation(problem.shape[0])
 
         solution = np.zeros(num_groups, dtype=int)
-        c = 0
+        current_fold = 0
         fold_total = 0
         for i in indices:
             group = np.sum(problem[i, :])
             if fold_total + group < total / k:
                 fold_total += group
             else:
-                c = (c + 1) % k
+                current_fold = (current_fold + 1) % k
                 fold_total = group
-            solution[i] = c
+            solution[i] = current_fold
     elif algo == "random":
         rng = default_rng()
         solution = rng.integers(low=0, high=k, size=num_groups)
@@ -118,6 +150,22 @@ def solve(problem: np.ndarray,
           min_cost=1e-5,
           max_retry=100,
           verbose=False) -> np.ndarray:
+    """Solve the problem.
+
+    Args:
+        problem (np.ndarray):
+        k (int): Number of folds, default 5.
+        min_cost (float): The largest the cost can be for an
+            acceptable solution. Default 1e-5.
+        max_retry (int): The max amount of times the program will
+            attempt to alter the current solution for a more
+            optimal one.
+        verbose (bool): True for more debug prints, defaults to False.
+
+    Returns:
+        np.ndarray: Optimized solution as a 1D array where each
+            value is the fold allocation for each group.
+    """
     hist = set()
     retry = 0
 
@@ -147,36 +195,24 @@ def solve(problem: np.ndarray,
 def select_move(decision: np.ndarray,
                 solution: np.ndarray,
                 history: Set) -> Tuple:
+    """Select the change to make to the current solution.
+
+    Args:
+        decision (np.ndarray): The current search space matrix.
+        solution (np.ndarray): The current solution.
+        history (Set): Previous solutions.
+    Returns:
+        Tuple: Position in the solution matrix to move a group
+            into a different fold.
+    """
     candidates = np.argsort(decision, axis=None)
 
-    for c in candidates:
-        p = np.unravel_index(c, decision.shape)
-        s = solution.copy()
-        s[p[0]] = p[1]
-        sol_str = solution_to_str(s)
+    for candidate in candidates:
+        position = np.unravel_index(candidate, decision.shape)
+        sol = solution.copy()
+        sol[position[0]] = position[1]
+        sol_str = solution_to_str(sol)
 
         if sol_str not in history:
-            return p
+            return position[0], position[1]
     return -1, -1  # No move found!
-
-
-def main():
-    problem = generate_problem(num_groups=500,
-                               num_classes=4,
-                               min_group_size=400,
-                               max_group_size=2000,
-                               class_percent=np.array([0.4, 0.3, 0.2, 0.1]))
-    solution = solve(problem, k=5, verbose=True)
-
-    print(np.sum(problem, axis=0) / np.sum(problem))
-    print()
-
-    folds = [problem[solution == i] for i in range(5)]
-    fold_percents = np.array([np.sum(folds[i], axis=0) / np.sum(folds[i]) for i in range(5)])
-    print(fold_percents)
-    print()
-    print([np.sum(folds[i]) / np.sum(problem) for i in range(5)])
-
-
-if __name__ == "__main__":
-    main()
