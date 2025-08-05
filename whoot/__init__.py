5ccf03db623657875554f4eef4611e31190b53a2
Katie Garwood <kgarwood@sdzwa.org>
2025-08-05 16:41:50 -0700

    add function needed to init instead because it might be useful
    the window expansion function could be useful elsewhere so in an
    attempt to packagize im putting it here and it could be useful when
    preparing other datasets later

--- whoot/__init__.py
+++ whoot/__init__.py
@@ -1,1 +1,3 @@
 __version__ = "0.0.2.dev0"
+
+from .audio_utils.py import expand_window
---
+++ whoot/audio_utils.py
@@ -0,0 +1,43 @@
+"""Functions to aid in audio data pre or post processing.
+
+"""
+from pydub import AudioSegment
+import random
+
+
+def expand_window(audio, start_time, end_time, length=3000, random=False):
+    """Expand the window size of a detection.
+
+    If you have audio segments with a designated start and stop time
+    for a detection, you can choose to expand the window to generate
+    an expanded detection window audio clip, clips longer than the desired
+    length will neither be expanded or shortened.
+
+    Args:
+        audio (AudioSegment.from_wav): An audio segment object.
+        start_time (float): Millisecond in wav file where the detection began.
+        end_time (float): Millisecond in wav file where detection ended.
+        length (int): Duration in millisecond of the desired window length.
+        random (bool): If the window expansion will have the detection centered 
+            or have randomized beginning and end window expansion.
+
+    Returns:
+        AudioSegment: The audio sample with expanded window of the detection.
+    """
+    duration = end_time - start_time
+    if duration > length:
+        segment = audio[start_time:end_time]
+    if random:
+        diff = length-duration
+        offset = random.uniform(0, diff)
+        new_start = start_time-offset 
+        end_offset = length-(offset+duration)
+        new_end = end_time + end_offset 
+        segment = audio[new_start:new_end] 
+    else:
+        half_diff = (length - duration)/2
+        expanded_start = start_time - half_diff
+        expanded_end = end_time + half_diff
+        segment = audio[expanded_start:expanded_end]
+
+    return segment
