443bdc617410ef8ae649a3b9d212d9efefb11dc4
Sean1572 <shperry@ucsd.edu>
2025-08-08 15:43:59 -0700

    Fix scripts gitignore bug
    Allows for script to be scripts

--- script/convert_flac.py
+++
@@ -1,8 +0,0 @@
-from pydub import AudioSegment
-import glob
-
-audio_files = glob.glob("data/burrowing_owl_dataset/audio/*")
-
-for file in audio_files:
-    song = AudioSegment.from_wav(file)
-    song.export(file.replace(".wav", ".flac").replace("audio", "audio_flac"), format = "flac")
