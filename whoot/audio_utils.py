1e267cc36a5619e597d3c10c4615378ef5d2f224
Katie Garwood <kgarwood@sdzwa.org>
2025-08-25 15:19:07 -0700

    linting

--- whoot/audio_utils.py
+++ whoot/audio_utils.py
@@ -1,8 +1,9 @@
 """Functions to aid in audio data pre or post processing.
 
 """
-from pydub import AudioSegment
 import random
+from pydub import AudioSegment
+from pydub.exceptions import TooManyMissingFrames
 
 
 def expand_window(audio, start_time, end_time, length=3000, randomize=False):
@@ -18,8 +19,8 @@ def expand_window(audio, start_time, end_time, length=3000, randomize=False):
         start_time (float): Millisecond in wav file where the detection began.
         end_time (float): Millisecond in wav file where detection ended.
         length (int): Duration in millisecond of the desired window length.
-        randomize (bool): If the window expansion will have the detection centered 
-            or have randomized beginning and end window expansion.
+        randomize (bool): If the window expansion will have the detection
+            centered or have randomized beginning and end window expansion.
 
     Returns:
         AudioSegment: The audio sample with expanded window of the detection.
@@ -30,14 +31,14 @@ def expand_window(audio, start_time, end_time, length=3000, randomize=False):
     if randomize:
         diff = length-duration
         offset = random.uniform(0, diff)
-        new_start = start_time-offset 
+        new_start = start_time-offset
         end_offset = length-(offset+duration)
         new_end = end_time + end_offset
-        try: 
+        try:
             segment = audio[new_start:new_end]
-        except Exception as e:
+        except TooManyMissingFrames:
             segment = audio[start_time:end_time]
-            print(f"audio segment is not long enough to window expand")
+            print("audio segment is not long enough to window expand")
     else:
         half_diff = (length - duration)/2
         expanded_start = start_time - half_diff
