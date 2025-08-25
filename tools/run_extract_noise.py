6a01724b8e6050ab96ab55b1117d86f1645fdea8
Katie Garwood <kgarwood@sdzwa.org>
2025-08-25 14:42:21 -0700

    remove unneeded line

--- tools/run_extract_noise.py
+++ tools/run_extract_noise.py
@@ -31,7 +31,6 @@ if __name__ == "__main__":
     with open(ARGS.config, 'r', encoding='UTF-8') as f:
         config = yaml.safe_load(f)
     all_files = os.listdir(config['audio'])
-    out = config['out']
     for file in all_files:
         try:
             print(f"running {file}")
