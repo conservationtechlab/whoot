74ba1c98f7bb448c9e71aacb8fe91ad98be57de9
Katie Garwood <kgarwood@sdzwa.org>
2026-09-04 11:45:46 -0700

    Fix flake8 and pylint and expand upon docstrings

--- data_exporters/label_studio_exporter/expand_birdnet_detections.py
+++ data_exporters/label_studio_exporter/expand_birdnet_detections.py
@@ -1,16 +1,26 @@
 """Expand birdnet detections into longer clips for validation.
 
-This script takes in t combined table of birdnet detections for a given
+This script takes in the combined table of birdnet detections for a given
 group of wav files. It checks for overlap, and then creates an expanded clip
 in order to provide acoustic context while validating the birdnet detections.
 It also outputs a pkl file containing a dictionary of audio info including
 the offset and duration of the label, as well as the label for each clip.
+This can be directly used to upload the birdnet start/stop labels in
+labelstudio. The metadata.csv can be used to map the shortened segments
+back to the longer original files to be able to create window
+expanded segments from strongly labeled data once it has been labeled
+in labelstudio or elsewhere.
+
+Usage:
+    python3 expand_birdnet_detections.py
+     -results /path/to/birdnet/combined/results.csv
+     -output /path/to/desired/output/folder/
 
 """
 import argparse
+import pickle
 from whoot import check_overlap_dict
 import pandas as pd
-import pickle
 
 
 if __name__ == "__main__":
@@ -25,7 +35,8 @@ if __name__ == "__main__":
     results = pd.read_csv(ARGS.results)
     out_dir = ARGS.output
 
-    results['File'] = results['File'].str.replace('Volumes/BUOW', 'mnt/restorage')
+    results['File'] = results['File'].str.replace('Volumes/BUOW',
+                                                  'mnt/restorage')
 
     all_data = {
         "audio": [],
@@ -34,9 +45,11 @@ if __name__ == "__main__":
 
     metadata = []
 
-    # creates shortened segments that combine overlaps into 1, provides results in dataframe
+    # creates shortened segments that combine overlaps into 1
     for file_path, detections in results.groupby("File"):
-        metadata_dict, metadata_list = check_overlap_dict(file_path, detections, out_dir)
+        metadata_dict, metadata_list = check_overlap_dict(file_path,
+                                                          detections,
+                                                          out_dir)
 
         all_data["audio"].extend(metadata_dict["audio"])
         all_data["labels"].extend(metadata_dict["labels"])
